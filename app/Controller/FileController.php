ba9c67866d4a06b6eeba6cd8c5fc2b2b1577b5d2
Dinh Quan <dinhquan@Dinhs-MacBook-Pro.local>
2014-03-05 00:49:58 +0700

    Upload File

--- app/Controller/FileController.php
+++ app/Controller/FileController.php
@@ -1,24 +1,18 @@
 <?php
 class FileController extends AppController {
 
-	var $name = 'Galleries';
-	var $components = array('Uploader.Uploader');
+	public $name = 'File';
 	
-	function beforeFilter(){
-        $this->Uploader->uploadDir = '\files\uploads\\'; //thu muc chua file upload
-        $this->Uploader->enableUpload = true;
-        $this->Uploader->maxFileSize = '2M'; // quy dinh dung luong duoc upload len toi da la 2 Megabytes
-        $this->Uploader->maxNameLength = 25;//do dai cua ten file
-        $this->Uploader->tempDir = TMP;
+	function beforeFilter() {
+        $this->pageTitle = 'Upload Files';
+        $this->layout = 'template';
+        return parent::beforeFilter();
     }
-	function upload() {
-		if (!empty($this->data)) {
-			 if ($data = $this->Uploader->upload('fileName')) {
-				echo "Upload thanh cong";
-			//do something;
-			 }else{
-			echo "Co loi xay ra! Vui long thu lai!";
-		 }	
+	public function index() {
+		if ($this->request->is('post')){
+			if ($this->File->save($this->request->data, true)) {
+				// Do something
+			}	
 		}
 	}
 }
--- app/Model/File.php
+++ app/Model/File.php
@@ -2,31 +2,25 @@
 
 class File extends AppModel {
 
-	public $actsAs = array(
-		'Uploader.Attachment' => array(
-			// Do not copy all these settings, it's merely an example
-			'FileLink' => array(
-				'nameCallback' => '',
-				'append' => '',
-				'prepend' => '',
-				'tempDir' => TMP,
-				'uploadDir' => '',
-				'transportDir' => '',
-				'finalPath' => '',
-				'dbColumn' => 'FileLink',
-				'metaColumns' => array(),
-				'defaultPath' => '',
-				'overwrite' => false,
-				'stopSave' => true,
-				'allowEmpty' => true,
-				'transforms' => array(),
-				'transformers' => array(),
-				'transport' => array(),
-				'transporters' => array(),
-				'curl' => array()
-			)
+public $actsAs = array(
+    'Uploader.Attachment' => array(
+        'image' => array(
+            'overwrite' => false,
+            'uploadDir' => 'uploads/jugyou',
+            'finalPath' => '',
+            'dbColumn' => 'image'
+        )
+    ),
+    'Uploader.FileValidation' => array(
+		'image' => array(
+			'minWidth' => 100,
+			'minHeight' => 100,
+			'extension' => array('gif', 'jpg', 'png', 'jpeg', 'pdf'),
+			'filesize' => 5242880,
+			'required' => true
 		)
-	);
+	)
+);
 }
 
 ?>
